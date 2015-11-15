5ad04b94a4043878daf58f24394a256ab0785c8d
aswathyd <aswathyd@gmail.com>
2015-11-14 23:35:57 -0500

    add destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ def index
     @articles = Article.all
   end
 def new
-  @article = Article.new
+@article = Article.new
 end
 def edit
   @article = Article.find(params[:id])
@@ -29,6 +29,9 @@ def show
     @article = Article.find(params[:id])
   end
 def destroy
+	@article = Article.find(params[:id])
+	@article.destroy
+	redirect_to articles_path
 end
  private 
  def article_params
